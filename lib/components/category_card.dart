f3ff88947b5845cbe1805d78d50bfeb2339147ee
Adam Behoun <xbehoua00@stud.fit.vutbr.cz>
2024-08-18 17:13:36 +0200

    Refactored home_page.dart

--- lib/components/category_card.dart
+++ lib/components/category_card.dart
@@ -1,32 +1,62 @@
 import 'package:flutter/material.dart';
-import '../screens/home_page.dart';
+
+class StyledText extends StatelessWidget {
+  final String text;
+
+  const StyledText({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 15.0,
+      ),
+    );
+  }
+}
 
 class CategoryCard extends StatelessWidget {
   final String age;
   final String? weight;
   final String? rank;
   final Function onDelete;
+  final bool showKata;
 
-  const CategoryCard({required Key key, required this.age, this.weight, this.rank, required this.onDelete}) : super(key: key);
+  const CategoryCard({required Key key, required this.age, this.weight, this.rank, required this.onDelete, required this.showKata}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return Card(
       elevation: 5.0,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
       ),
-      child: Column(
+      child: Stack(
         children: <Widget>[
-          Text('Age: $age'),
-          showKata ? Text('Rank: $rank') : Text('Weight: $weight'),
-          IconButton(
-            icon: const Icon(Icons.delete),
-            onPressed: () => onDelete(key),
-          )
-        ]
-      )
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center( // Add this line
+                  child: Text(showKata ? 'Kata' : 'Kumite', style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500)), // Wrap this line
+                ), // Add this line
+                StyledText(text: 'Age: $age'),
+                showKata ? StyledText(text: 'Rank: $rank') : StyledText(text: 'Weight: $weight'),
+              ],
+            ),
+          ),
+          Positioned(
+            right: 10,
+            bottom: 10,
+            child: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () => onDelete(key),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/components/main_scaffold.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'load_button.dart';
+
+class MainScaffold extends StatelessWidget {
+  final Widget body;
+
+  MainScaffold({required this.body});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: const Offset(0, 3), // changes position of shadow
+              ),
+            ],
+          ),
+          child: AppBar(
+            title: const Row(
+              children: [
+                Icon(Icons.sports_martial_arts_rounded),
+                SizedBox(width: 8.0),
+                Text('Karate'),
+              ],
+            ),
+            actions: <Widget>[
+              createLoadButton(context),
+            ],
+            elevation: 0.0, // remove elevation from AppBar
+          ),
+        ),
+      ),
+      body: body,
+    );
+  }
+}
