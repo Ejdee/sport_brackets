625c707425f114650c3cc3eb3a525e257a63df4f
Adam Behoun <xbehoua00@stud.fit.vutbr.cz>
2024-08-27 19:54:13 +0200

    feat: simple bracket generator for 2^n participants

--- lib/components/single_bracket.dart
+++ lib/components/single_bracket.dart
@@ -1,11 +1,23 @@
 import 'package:pdf/widgets.dart' as pw;
 
+pw.Container buildDoubleBracketPdf(String participant1, String participant2) {
+  return pw.Container(
+    width: 130, // Set a fixed width for the double bracket container
+    child: pw.Column(
+      children: [
+        buildSingleBracketPdf(participant1),
+        buildSingleBracketPdf(participant2),
+      ],
+    ),
+  );
+}
+
 pw.Container buildSingleBracketPdf(String participant) {
   return pw.Container(
+    width: 130, // Set a fixed width for the single bracket container
     padding: pw.EdgeInsets.all(8),
     decoration: pw.BoxDecoration(
       border: pw.Border.all(),
-      borderRadius: pw.BorderRadius.circular(8),
     ),
     child: pw.Text(participant),
   );
