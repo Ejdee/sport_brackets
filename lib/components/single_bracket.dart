d29efd96621f706da68a3154f0cb094f2f5235f6
Adam Behoun <xbehoua00@stud.fit.vutbr.cz>
2024-08-29 19:57:16 +0200

    feat; improved participant brackets layout

--- lib/components/single_bracket.dart
+++ lib/components/single_bracket.dart
@@ -1,8 +1,9 @@
 import 'package:pdf/widgets.dart' as pw;
 
-pw.Container buildDoubleBracketPdf(String participant1, String participant2) {
+pw.Container buildDoubleBracketPdf(String participant1, String participant2, double margin) {
   return pw.Container(
     width: 130, // Set a fixed width for the double bracket container
+    margin: pw.EdgeInsets.only(bottom: margin),
     child: pw.Column(
       children: [
         buildSingleBracketPdf(participant1),
@@ -15,7 +16,8 @@ pw.Container buildDoubleBracketPdf(String participant1, String participant2) {
 pw.Container buildSingleBracketPdf(String participant) {
   return pw.Container(
     width: 130, // Set a fixed width for the single bracket container
-    padding: pw.EdgeInsets.all(8),
+    height: 15,
+    padding: pw.EdgeInsets.only(left: 2),
     decoration: pw.BoxDecoration(
       border: pw.Border.all(),
     ),
